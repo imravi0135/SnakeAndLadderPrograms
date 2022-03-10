a15813c0b2cda8e8d8c79a0ba133036a3e96a536
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-10 14:44:54 +0530

    Play Till End

--- SnakeAndLadderPrograms/SnakeAndLadder.cs
+++ SnakeAndLadderPrograms/SnakeAndLadder.cs
@@ -8,36 +8,45 @@ namespace SnakeAndLadderPrograms
 {
     internal class SnakeAndLadder
     {
-
-        public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1;
+        public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1, WINNING = 100;
         public int StartPoint = 0;
         public int DiceRoll;
         int Option = 0;
         public void StartPlaying()
         {
-            Random Random = new Random();
-            DiceRoll = Random.Next(1, 7);
-            Console.WriteLine("player Roll  Dice number is:" + " " + DiceRoll);
-            Option = Random.Next(0, 3);
-            Console.WriteLine("Dice value:" + Option);
-            switch (Option)
+            while (this.StartPoint < 100)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No Play");
-                    break;
-                case LADDER:
-                    StartPoint += DiceRoll;
-                    Console.WriteLine("Got Ladder:" + StartPoint);
-                    break;
-                case SNAKE:
-                    StartPoint -= DiceRoll;
-                    Console.WriteLine("Snake Attack:" + StartPoint);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option");
-                    break;
+                Random Random = new Random();
+                DiceRoll = Random.Next(1, 7);
+                Console.WriteLine("player Roll  Dice number is:" + " " + DiceRoll);
+                Option = Random.Next(0, 3);
+                Console.WriteLine("Dice value:" + Option);
+                switch (Option)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
+                    case LADDER:
+                        StartPoint += DiceRoll;
+                        Console.WriteLine("Got Ladder:" + StartPoint);
+                        break;
+                    case SNAKE:
+                        StartPoint -= DiceRoll;
+                        Console.WriteLine("Snake Attack:" + StartPoint);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                if (StartPoint < 0)
+                {
+                    StartPoint = 0;
+                }
             }
         }
+
+
+
     }
 
 }
